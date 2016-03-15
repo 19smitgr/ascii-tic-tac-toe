6e71329c1b103aef7f2412833c5f075ad7ecb90b
Garrett Smith <garrett_smith@aol.com>
2016-03-15 06:01:17 -0400

    Add rules to win

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Please refrain from making the program more efficient, as I am still working on it.  
 
-I need to add rules to win and AI for the game to be complete. Once that is complete, I might make a 4x4 version of tic-tac-toe if that will make the game any more fun.   
+I need to add  AI for the game to be complete. Once that is complete, I might make a 4x4 version of tic-tac-toe if that will make the game any more fun.   
 
 This project uses Python 3
--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,7 +5,6 @@
 		--See bottom for more information--
 
 """
-from __future__ import print_function, division
 
 #tic-tac-toe board split into an array
 #extra "" added at the beginning so the iteration starts at 1
@@ -13,6 +12,13 @@ grid = ["", "    A  ", "   B   ", "  C  ", "       ", "|     ", "|     ", "1    
 
 #Grid spaces that haven't been used; gets modified at the end of script
 unused_grid_spaces = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+
+#arrays for recording which spaces have been played in from each letter: 'x' and 'o'
+spaces_that_x_has_played_in = []
+spaces_that_o_has_played_in = []
+
+#3D array of all winning combinations
+winning_combinations = [["A1", "B2", "C3"], ["A3", "B2", "C1"], ["A1", "A2", "A3"], ["B1", "B2", "B3"], ["C1", "C2", "C3"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"]]
 
 def turn(round_number):
 
@@ -30,7 +36,6 @@ def turn(round_number):
 		return divisible_by_certain_number
 
 	#this method paints the grid, and inserts an endline every third item
-	#by default, print() prints an endline.  By specifying end = "", no newline is created
 	def paint_grid():
 		for item in range(0,31):
 			check_if_is_divisible_by_a_certain_number(item, 3)
@@ -38,6 +43,7 @@ def turn(round_number):
 			if divisible_by_certain_number and item != 0:
 				print(grid[item])
 			else:
+				#by default, print() prints an endline.  By specifying end = "", no newline is created
 				print(grid[item], end = "")
 
 	#runs the print_grid() function and paints the board on first run
@@ -50,6 +56,13 @@ def turn(round_number):
 
 		#checks to see if the user input matches any of the acceptable responses
 		if play_in_what_space in unused_grid_spaces:
+
+			#Records which spaces x and o have played in
+			if check_if_is_divisible_by_a_certain_number(round_number, 2):
+				spaces_that_o_has_played_in.append(play_in_what_space)
+			else:
+				spaces_that_x_has_played_in.append(play_in_what_space)
+			
 			break
 		else:
 			print("\nThat answer is not valid. Are you typing the grid spot correctly?\nEx. B3\n")
@@ -77,9 +90,12 @@ def turn(round_number):
 
 	def insert_letter_into_square_in_grid(space_in_the_grid_the_user_picked):
 
+		#spaces 7,16, and 25 are the squares on the left column;
+		#They have a different number of spaces in the grid than the other 2 columns and have to be inserted differently
 		if space_in_the_grid_the_user_picked == 7 or space_in_the_grid_the_user_picked == 16 or space_in_the_grid_the_user_picked == 25:
 
 			#sets the following variable equal to the grid space the user choose with an "x" or an "o" in it
+			#switches x and o every turn
 			if check_if_is_divisible_by_a_certain_number(round_number, 2):		
 				box_with_letter_inserted_into_it = insert(grid[space_in_the_grid_the_user_picked], "o", 4)
 			else:
@@ -116,12 +132,43 @@ def turn(round_number):
 #set default round equal to zero
 round = 0
 
+#set default to not winning
+win = False
+
+
+def winning_rules(winning_combination):
+	if set(winning_combinations[winning_combination]).issubset(spaces_that_o_has_played_in):
+		print("\n'o' wins!")
+		return True
+	elif set(winning_combinations[winning_combination]).issubset(spaces_that_x_has_played_in):
+		print("\n'x' wins!")
+		return True
+	else:
+		return False
+
+
 while unused_grid_spaces:
+
+
+
 	#When the program runs, the round will become 1 and the program will start
 	round += 1
-	turn(round)
 
-	
+	for one_through_eight in range(0,7):
+		
+		win_or_not = winning_rules(one_through_eight)
+		if win_or_not:
+			global win
+
+			win = True
+			break
+
+	if win:
+		break
+	elif win == False:
+		turn(round)
+
+
 
 """
 Mini-sketch:
